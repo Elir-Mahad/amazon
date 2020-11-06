b70e845a0296b7e82d0279cec7dab3e1ebb09d88
elir-mahad <mmahad280@gmail.com>
2020-11-06 15:41:46 -0500

    Updates

---
+++ precache-manifest.526031b02522639fadc3f469d268389b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1250097a21d2edf10ce82f7e2efff330",
+    "url": "/amazon/index.html"
+  },
+  {
+    "revision": "54adeb90ab87d919be8c",
+    "url": "/amazon/static/css/main.23bde67d.chunk.css"
+  },
+  {
+    "revision": "8d82785479304a897df1",
+    "url": "/amazon/static/js/2.e2125458.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/amazon/static/js/2.e2125458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54adeb90ab87d919be8c",
+    "url": "/amazon/static/js/main.e2890fe2.chunk.js"
+  },
+  {
+    "revision": "bf5e2e0c760cd7aa92cd",
+    "url": "/amazon/static/js/runtime-main.c52d3dae.js"
+  }
+]);
